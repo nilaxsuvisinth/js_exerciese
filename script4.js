c8208795ad3a8d32c11a8e6b33030aca3cdb775c
nilaxsuvisinth <nilaxsuvisinth27@gmail.com>
2023-10-27 13:04:55 +0530

    loop commit

---
+++ script4.js
@@ -0,0 +1,23 @@
+//loop
+
+for(var i=1; i<10; i+=2){
+    console.log(i)
+}
+
+//01 <=====> Create a for loop to print “Hi John” 5 times in the console.
+
+for (let i = 0; i < 5; i++) {
+    console.log("Hi John");
+}
+
+
+
+// do while loop
+x=0
+i=0
+
+do{
+    x=x+i;
+    console.log(x)
+    i++;
+} while(i<5);
